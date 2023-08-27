ad5feb831723e55256488494d025c82fd307df57
Vladislav Krinitsyn <grosseclay@gmail.com>
2023-08-28 02:15:42 +0500

    прошли семинар 7

---
+++ семинары/sem7/Program.cs
@@ -0,0 +1,226 @@
+﻿// Task 1. Задайте двумерный массив размером m×n, заполненный случайными целыми числами.
+/*
+int[,] CreateRandom2dArray()
+{
+    Console.WriteLine("Введите колличество строк: ");
+    int rows = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите колличество столбцов: ");
+    int colums = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите минимальное значение: ");
+    int minValue = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите максимальное значение: ");
+    int maxValue = Convert.ToInt32(Console.ReadLine());
+
+
+    int[,] array = new int[rows, colums];
+    for(int i = 0; i < rows; i++)
+        for(int j = 0; j < colums; j++)
+            array[i,j] = new Random().Next(minValue, maxValue + 1);
+    
+    return array;
+}
+
+void Show2dArray (int[,] array)
+{
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+            Console.Write(array[i,j] + " ");
+
+        Console.WriteLine();
+    }
+}
+
+int[,] myArray = CreateRandom2dArray();
+Show2dArray(myArray);
+*/
+
+// Task 2. Задайте двумерный массив размера m на n, каждый элемент в массиве находится по формуле: Aij = i+j. Выведите полученный массив на экран.
+/*
+int[,] Create2dArray()
+{
+    Console.WriteLine("Введите колличество строк: ");
+    int rows = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите колличество столбцов: ");
+    int colums = Convert.ToInt32(Console.ReadLine());
+
+    int[,] array = new int[rows, colums];
+    for(int i = 0; i < rows; i++)
+        for(int j = 0; j < colums; j++)
+            array[i,j] = i + j;
+    
+    return array;
+}
+
+void Show2dArray (int[,] array)
+{
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+            Console.Write(array[i,j] + " ");
+
+        Console.WriteLine();
+    }
+}
+
+int[,] myArray = Create2dArray();
+Show2dArray(myArray);
+*/
+
+// Task 3. Задайте двумерный массив. Найдите элементы, у которых оба индекса чётные, и замените эти элементы на их квадраты.
+/*
+int[,] CreateRandom2dArray()
+{
+    Console.WriteLine("Введите колличество строк: ");
+    int rows = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите колличество столбцов: ");
+    int colums = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите минимальное значение: ");
+    int minValue = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите максимальное значение: ");
+    int maxValue = Convert.ToInt32(Console.ReadLine());
+
+
+    int[,] array = new int[rows, colums];
+    for(int i = 0; i < rows; i++)
+        for(int j = 0; j < colums; j++)
+            array[i,j] = new Random().Next(minValue, maxValue + 1);
+    
+    return array;
+}
+
+void Zamena(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i+=2)
+        for (int j = 0; j < array.GetLength(1); j+=2)
+            array[i, j] = array[i, j] * array[i, j];
+}
+
+void Show2dArray (int[,] array)
+{
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+            Console.Write(array[i,j] + " ");
+
+        Console.WriteLine();
+    }
+}
+
+int[,] myArray = CreateRandom2dArray();
+Show2dArray(myArray);
+Console.WriteLine();
+Zamena(myArray);
+Show2dArray(myArray);
+*/
+
+// Task 4. Задайте двумерный массив. Найдите сумму элементов, находящихся на главной диагонали (с индексами (0,0); (1;1) и т.д.
+/*
+int[,] CreateRandom2dArray()
+{
+    Console.WriteLine("Введите колличество строк: ");
+    int rows = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите колличество столбцов: ");
+    int colums = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите минимальное значение: ");
+    int minValue = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите максимальное значение: ");
+    int maxValue = Convert.ToInt32(Console.ReadLine());
+
+
+    int[,] array = new int[rows, colums];
+    for(int i = 0; i < rows; i++)
+        for(int j = 0; j < colums; j++)
+            array[i,j] = new Random().Next(minValue, maxValue + 1);
+    
+    return array;
+}
+
+int SumDiagonal(int [,] array)
+{
+    int y = 0;
+    int sum = 0;
+    while (y < array.GetLength(0) && y < array.GetLength(1))
+    {
+        sum += array[y,y];
+        y++;
+    }
+    return sum;
+}
+
+void Show2dArray (int[,] array)
+{
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+            Console.Write(array[i,j] + " ");
+
+        Console.WriteLine();
+    }
+}
+
+int[,] myArray = CreateRandom2dArray();
+Show2dArray(myArray);
+Console.WriteLine();
+Console.WriteLine(SumDiagonal(myArray));
+*/
+
+// Task $. Сортировка по убыванию
+/*
+int[,] CreateRandom2dArray()
+{
+    Console.WriteLine("Введите колличество строк: ");
+    int rows = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите колличество столбцов: ");
+    int colums = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите минимальное значение: ");
+    int minValue = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите максимальное значение: ");
+    int maxValue = Convert.ToInt32(Console.ReadLine());
+
+
+    int[,] array = new int[rows, colums];
+    for(int i = 0; i < rows; i++)
+        for(int j = 0; j < colums; j++)
+            array[i,j] = new Random().Next(minValue, maxValue + 1);
+    
+    return array;
+}
+
+void Show2dArray (int[,] array)
+{
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+            Console.Write(array[i,j] + " ");
+
+        Console.WriteLine();
+    }
+}
+
+void Sort(int[,] array)
+{
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1) - 1; j++)
+        {
+            for(int k = 0; k < array.GetLength(1) - 1 ; k++)
+            {
+                if (array[i,k] < array[i,k +1])
+                {
+                    int temp = array[i,k];
+                    array[i,k] = array[i,k+1];
+                    array[i,k+1] = temp;
+                }
+            }
+        }
+    }
+}
+
+int[,] myArray = CreateRandom2dArray();
+Show2dArray(myArray);
+Console.WriteLine();
+
+Sort(myArray);
+Show2dArray(myArray);
+*/
