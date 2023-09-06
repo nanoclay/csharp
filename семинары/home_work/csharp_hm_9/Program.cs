9e88d7746daee778234b15ca85f265d242c375cf
Vladislav Krinitsyn <grosseclay@gmail.com>
2023-09-07 02:18:59 +0500

    сделана домашняя работа 9 семинара

---
+++ семинары/home_work/csharp_hm_9/Program.cs
@@ -0,0 +1,69 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+/*
+void PrintNumber(int num)
+{
+    if (num == 1)
+        Console.WriteLine(num);
+
+    else
+    {
+        Console.Write(num + ", ");
+        PrintNumber(num - 1);
+    }
+}
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+PrintNumber(number);
+*/
+
+// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+/*
+int PrintNumber(int m, int n, int sum)
+{
+    if (m > n)
+        return sum;
+
+    else
+    {
+        sum = sum + m ;
+        m++;
+        return PrintNumber(m, n, sum);
+    }
+}
+
+Console.Write("Введите первое число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int mum = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(PrintNumber(num, mum, 0));
+*/
+
+// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+
+int Akkerman(int n, int m)
+{
+  if (n == 0)
+    return m + 1;
+  else
+    if ((n != 0) && (m == 0))
+      return Akkerman(n - 1, 1);
+    else
+      return Akkerman(n - 1, Akkerman(n, m - 1));
+}
+
+Console.Write("Введите первое число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int mum = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(Akkerman(num, mum));
---
+++ семинары/sem9/Program.cs
@@ -0,0 +1,92 @@
+﻿// Задача 63: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от 1 до N.
+// N = 5 -> "1, 2, 3, 4, 5"
+// N = 6 -> "1, 2, 3, 4, 5, 6"
+
+/*
+void PrintNumber(int num, int n = 1)
+{
+    if (num == n)
+        Console.WriteLine(num);
+
+    else
+    {
+        Console.Write(n + ", ");
+        PrintNumber(num, n + 1);
+    }
+}
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+PrintNumber(number, 1);
+*/
+
+// Задача 65: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+// M = 1; N = 5 -> "1, 2, 3, 4, 5"
+// M = 4; N = 8 -> "4, 6, 7, 8"
+
+/*
+void PrintNumber(int m, int n)
+{
+    if (m == n)
+        Console.WriteLine(m);
+
+    else
+    {
+        Console.Write(m + ", ");
+        PrintNumber(m + 1, n);
+    }
+}
+
+Console.Write("Введите первое число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int mum = Convert.ToInt32(Console.ReadLine());
+
+PrintNumber(num, mum);
+*/
+
+// Задача 69: Напишите программу, которая на вход принимает два числа A и B, и возводит число А в целую степень B с помощью рекурсии.
+// A = 3; B = 5 -> 243 (3⁵)
+// A = 2; B = 3 -> 8
+
+/*
+int PowerAB(int a, int b, int sum)
+{
+    if (b < 1)
+       return sum;
+    else
+    {
+        sum = sum * a;
+        b--;
+        return PowerAB(a, b, sum);
+    }
+}
+
+Console.Write("Введите первое число: ");
+int aNum = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int bNum = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(PowerAB(aNum, bNum, 1));
+*/
+
+// Задача 67: Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр.
+// 453 -> 12
+// 45 -> 9
+
+int SumDigits(int number, int start)
+{
+
+    if(number == 0)
+        return start;
+    else
+    {
+        start = start + (number % 10);
+        number = number / 10;
+        return SumDigits(number, start);
+    }
+}
+
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(SumDigits(num, 0));
